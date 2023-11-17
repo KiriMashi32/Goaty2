46c3163c3533778934b3881d7ee5d386bc929c48
Kiri <46458337+KiriMashi32@users.noreply.github.com>
2023-11-17 16:46:37 +0100

    owner

--- resources/views/goats/show.blade.php
+++ resources/views/goats/show.blade.php
@@ -7,12 +7,14 @@
         <img src="/img/{{$goat->image_path}}" alt="image de chèvre" class="mb-4 rounded-lg">
         <a href='/goats/{{$goat->id}}/edit' class="text-yellow-300 hover:text-yellow-500">Modifier {{$goat->name}} </a>
         <ul class="text-lg list-disc list-inside mt-4">
-            <li><strong>Id:</strong> {{$goat->id}}</li>
-            <li><strong>Sexe:</strong> {{$goat->sex ? 'Femelle' : 'Male'}}</li>
-            <li><strong>Nom:</strong> {{$goat->name}}</li>
-            <li><strong>Date de naissance:</strong> {{$goat->birthday}}</li>
-            <li><strong>Couleur:</strong> {{$goat->color}}</li>
-            <li><strong>Prix:</strong> {{$goat->price}}</li>
+            <li><strong>Id : </strong> {{$goat->id}}</li>
+            {{-- <li><strong>Proprio : </strong>{{App\Models\User::find($goat->user_id)->name}} </li> --}}
+            <li><strong>Proprio : </strong>{{$goat->owner->name}}</li>
+            <li><strong>Sexe : </strong> {{$goat->sex ? 'Femelle' : 'Male'}}</li>
+            <li><strong>Nom : </strong> {{$goat->name}}</li>
+            <li><strong>Date de naissance : </strong> {{$goat->birthday}}</li>
+            <li><strong>Couleur : </strong> {{$goat->color}}</li>
+            <li><strong>Prix : </strong> {{$goat->price}}</li>
         </ul>
         <a href="/goats" class="text-yellow-300 hover:text-yellow-500 block mt-4">All the goats</a>
     </div>
